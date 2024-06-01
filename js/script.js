1d04cc377119e29da79e1418030cb4b0a92cfded
Gabriel Zanardi Nogueira <159185296+Gabriel-Zanardi@users.noreply.github.com>
2024-06-01 14:16:06 +0000

    included favicons and started to work on the API inclusion

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Gabriel
+// Created on: June 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+async function myButtonClicked() {
+  try {
+    const resultJSON = await fetch("https://api.nasa.gov/planetary/apod?api_key=A5F9DYnidDu2IzQoEJbdLi07DiNHlMYKYLPV5VIu")
+    const jsonData = await resultJSON.json()
+    //console.log(jsonData)
+    const picOfTheDay = jsonData.url
+    console.log(picOfTheDay)
+    // then show
+    document.getElementById("final-answer").innerHTML = picOfTheDay
+  } catch (error) {
+    console.error(error)
+  }
+}
